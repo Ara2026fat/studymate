2b49c055e1a2a4b82505064c1a91c9b6bfb082f7
Ara2026fat <ara1979fat@gmail.com>
2026-09-16 21:35:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
    change between builds and re-fetching them wastes a student's data.
 */
 
-const BUILD = "2026-09-15-01";
+const BUILD = "2026-09-16-06";
 const CACHE = `studymate-${BUILD}`;
 const SHELL = ["./", "./index.html", "./manifest.json"];
 
